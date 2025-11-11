526805d5e5bc0e5b37a92520f2291edd066ba933
may raz <mmayr112233@gmail.com>
2025-11-12 00:35:53 +0200

    server code in comment

--- server.py
+++ server.py
@@ -29,3 +29,54 @@ while True:
 
     server_socket.sendto(response.encode(), addr)
     print(f"Sent response: {response}\n")
+
+# import socket
+# import sys
+
+# def load_zone(file_name):
+#     zone = {}
+#     with open(file_name, 'r') as f:
+#         for line in f:
+#             line = line.strip()
+#             if line:
+#                 domain, ip, rec_type = line.split(',')
+#                 zone[domain] = (rec_type, ip)
+#     return zone
+
+
+# def main():
+#     if len(sys.argv) != 3:
+#         print("Usage: python server.py [myPort] [zoneFileName]")
+#         return
+    
+#     port = int(sys.argv[1])
+#     zone_file = sys.argv[2]
+#     # load zone file
+#     zone = load_zone(zone_file)
+
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     s.bind(('', port))
+#     print(f"Server listening on port {port}...")
+
+#     while True:
+#         data, addr = s.recvfrom(1024)
+#         domain = data.decode().strip()
+#         response = "non-existent domain"
+
+#         # check if domain exists in zone
+#         if domain in zone:
+#             rec_type, ip = zone[domain]
+#             response = f"{domain},{ip},{rec_type}"
+
+#         # NS
+#         # if domain not found, check for NS records for parent domains
+#         else:
+#             for z_domain, (rec_type, ip) in zone.items():
+#                 if rec_type == "NS" and domain.endswith(z_domain.strip('.')):
+#                     response = f"{z_domain},{ip},NS"
+#                     break
+
+#         s.sendto(response.encode(), addr)
+
+# if __name__ == "__main__":
+#     main()
